3126c582bd2f0c83ce513ac7977c49a82c883274
Raul S Sorban <raulssorban@users.noreply.github.com>
2017-03-07 11:59:36 +0100

    FreeCamera update

--- FreeCamera.cs
+++ FreeCamera.cs
@@ -1,10 +1,10 @@
-﻿using UnityEngine;
+using UnityEngine;
 
 public class FreeCamera : MonoBehaviour
 {
     [Header("Target")]
-    public bool useTarget;
-    public float distance;
+    public bool targetMode;
+    public float distance = 2.5f;
     public Transform targetTransform;
 
     [Header("Speeds")]
@@ -15,16 +15,31 @@ public class FreeCamera : MonoBehaviour
     public bool useLerp = true;
     public float positionLerp = 7.5f;
     public float rotationLerp = 7.5f;
+    public float fieldOfViewLerp = 10.0f;
 
-    [Header("Keys")]
+    [Header("Field Of View")]
+    public float minFieldOfView = 20.0f;
+    public float maxFieldOfView = 120.0f;
+    public float middle = 0.55f;
+
+    [Header("Mouse KeyCodes")]
     public KeyCode mainKey = KeyCode.Mouse1;
     public KeyCode fieldOfViewKey = KeyCode.Mouse0;
     public KeyCode resetFieldOfViewKey = KeyCode.Mouse2;
 
+    [Header("Keyboard KeyCodes")]
+    public KeyCode forwardKey = KeyCode.W;
+    public KeyCode backwardKey = KeyCode.S;
+    public KeyCode rightKey = KeyCode.D;
+    public KeyCode leftKey = KeyCode.A;
+    public KeyCode panUpKey = KeyCode.Q;
+    public KeyCode panDownKey = KeyCode.E;
+    public KeyCode sprintKey = KeyCode.LeftShift;
+
     private Vector3 position = new Vector3();
     private Quaternion rotation = new Quaternion();
     private float x, y, currentFieldOfView, zoom;
-    private bool freeCamLock, targetLock;
+    private bool activated, freeCamLock, targetLock;
     private Camera targetCamera;
     private float currentMovementSensitivity = 1;
     private float currentCameraSensitivity = 1;
@@ -33,14 +48,25 @@ public class FreeCamera : MonoBehaviour
     {
         targetCamera = GetComponent<Camera> ();
 
+        if (targetCamera == null)
+        {
+            Debug.LogError ("[Free Camera] The object that this script is on does not have a Camera component.");
+            activated = false;
+        }
+        else
+            activated = true;
+
         position = transform.position;
         rotation = transform.rotation;
-        x = transform.rotation.x;
-        y = transform.rotation.y;
-        zoom = 0.55f;
+        x = transform.localEulerAngles.y;
+        y = transform.localEulerAngles.x;
+        zoom = middle;
     }
     private void Update ()
     {
+        if (!activated)
+            return;
+
         Motor ();
 
         FreeCamInput ();
@@ -54,21 +80,21 @@ public class FreeCamera : MonoBehaviour
         //
         transform.position = useLerp ? Vector3.Lerp (transform.position, position, Time.deltaTime * positionLerp) : position;
         transform.rotation = useLerp ? Quaternion.Lerp (transform.rotation, rotation, Time.deltaTime * rotationLerp) : rotation;
-        targetCamera.fieldOfView = useLerp ? Mathf.Lerp (targetCamera.fieldOfView, Mathf.Lerp (20.0f, 120.0f, zoom), Time.deltaTime * 10) : Mathf.Lerp (20.0f, 120.0f, zoom);
+        targetCamera.fieldOfView = useLerp ? Mathf.Lerp (targetCamera.fieldOfView, Mathf.Lerp (minFieldOfView, maxFieldOfView, zoom), Time.deltaTime * fieldOfViewLerp) : Mathf.Lerp (minFieldOfView, maxFieldOfView, zoom);
     }
     public void FreeCamInput ()
     {
-        freeCamLock = Input.GetKey (mainKey) && !useTarget;
+        freeCamLock = Input.GetKey (mainKey) && !targetMode;
 
         //
         // Cursor lockage
         //
-        if (freeCamLock && !useTarget)
+        if (freeCamLock && !targetMode)
         {
             Cursor.visible = false;
             Cursor.lockState = CursorLockMode.Locked;
         }
-        else if (!freeCamLock && !useTarget)
+        else if (!freeCamLock && !targetMode)
         {
             Cursor.visible = true;
             Cursor.lockState = CursorLockMode.None;
@@ -80,7 +106,7 @@ public class FreeCamera : MonoBehaviour
         if (freeCamLock && Input.GetKey (fieldOfViewKey))
             zoom = Mathf.Clamp (zoom + Input.GetAxisRaw ("Mouse Y") * -0.033f, 0.01f, 1.0f);
         if (freeCamLock && Input.GetKeyDown (resetFieldOfViewKey))
-            zoom = 0.55f;
+            zoom = middle;
 
         //
         // Input and motor
@@ -95,19 +121,19 @@ public class FreeCamera : MonoBehaviour
 
             rotation = Quaternion.Euler (y, x, 0);
 
-            if (Input.GetKey (KeyCode.W))
+            if (Input.GetKey (forwardKey))
                 position += transform.forward * Time.deltaTime * currentMovementSensitivity;
-            if (Input.GetKey (KeyCode.S))
+            if (Input.GetKey (backwardKey))
                 position -= transform.forward * Time.deltaTime * currentMovementSensitivity;
-            if (Input.GetKey (KeyCode.D))
+            if (Input.GetKey (rightKey))
                 position += transform.right * Time.deltaTime * currentMovementSensitivity;
-            if (Input.GetKey (KeyCode.A))
+            if (Input.GetKey (leftKey))
                 position -= transform.right * Time.deltaTime * currentMovementSensitivity;
-            if (Input.GetKey (KeyCode.X))
+            if (Input.GetKey (panUpKey))
                 position += transform.up * Time.deltaTime * currentMovementSensitivity;
-            if (Input.GetKey (KeyCode.C))
+            if (Input.GetKey (panDownKey))
                 position -= transform.up * Time.deltaTime * currentMovementSensitivity;
-            if (Input.GetKey (KeyCode.LeftShift))
+            if (Input.GetKey (sprintKey))
                 currentMovementSensitivity = sprintSpeed;
             else
                 currentMovementSensitivity = normalSpeed;
@@ -115,17 +141,23 @@ public class FreeCamera : MonoBehaviour
     }
     public void TargetMotorInput ()
     {
-        targetLock = Input.GetKey (mainKey) && useTarget;
+        targetLock = Input.GetKey (mainKey) && targetMode;
+
+        //
+        // Error masking
+        //
+        if (targetTransform == null)
+            targetMode = false;
 
         //
         // Cursor lockage
         //
-        if (targetLock && useTarget)
+        if (targetLock && targetMode)
         {
             Cursor.visible = false;
             Cursor.lockState = CursorLockMode.Locked;
         }
-        else if (!targetLock && useTarget)
+        else if (!targetLock && targetMode)
         {
             Cursor.visible = true;
             Cursor.lockState = CursorLockMode.None;
